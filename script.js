7609737150f28197927d49331a37125ee184af4f
Marek Wal <walmarek1987@gmail.com>
2022-11-14 14:02:15 +0100

    responsivity and js changed

--- script.js
+++ script.js
@@ -1,31 +1,32 @@
-console.log("Hello All. Just started developing");
+{
+  console.log("Hello All. Just started developing");
 
-const inputCurrenyElement = document.querySelector(".js-select-currencyInput");
-const outputCurrenyElement = document.querySelector(".js-select-currencyOutput");
-const inputElement = document.querySelector(".js-form-fieldInput");
-const outputElement = document.querySelector(".js-form-fieldOutput");
-const formElement = document.querySelector(".js-form");
+  const formElement = document.querySelector(".js-form");
 
-var select = document.getElementById("js-currencyInput");
-var option = select.options[select.selectedIndex];
+  formElement.addEventListener("input", () => {
+    const inputCurrenyElement = document.querySelector(
+      ".js-select-currencyInput"
+    );
+    const outputCurrenyElement = document.querySelector(
+      ".js-select-currencyOutput"
+    );
+    const inputElement = document.querySelector(".js-form-fieldInput");
+    const outputElement = document.querySelector(".js-form-fieldOutput");
 
-var select = document.getElementById("js-currencyOutput");
-var option = select.options[select.selectedIndex];
+    const inputCurrencyRate = inputCurrenyElement.value;
+    const outputCurrencyRate = outputCurrenyElement.value;
+    const inputAmount = inputElement.value;
 
-formElement.addEventListener("input", () => {
-  const inputCurrencyRate = inputCurrenyElement.value;
-  const outputCurrencyRate = outputCurrenyElement.value;
-  const inputAmount = inputElement.value;
+    const calculateResult = (inputAmount, inputRate, outputRate) => {
+      return (inputAmount * inputRate) / outputRate;
+    };
 
-  const calculateResult = (inputAmount, inputRate, outputRate) => {
-    return (inputAmount * inputRate) / outputRate;
-  };
+    const result = calculateResult(
+      inputAmount,
+      inputCurrencyRate,
+      outputCurrencyRate
+    );
 
-  const result = calculateResult(
-    inputAmount,
-    inputCurrencyRate,
-    outputCurrencyRate
-  );
-
-  outputElement.value = result.toFixed(2);
-});
+    outputElement.value = result.toFixed(2);
+  });
+}
